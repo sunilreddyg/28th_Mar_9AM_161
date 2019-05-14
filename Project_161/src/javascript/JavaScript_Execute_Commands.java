a45855ace731c399ac78c847d430f23c94b78b36
Administrator <Administrator@192.168.2.3>
2019-05-14 11:25:11 +0530

    wit commands

--- Project_161/src/javascript/JavaScript_Execute_Commands.java
+++ Project_161/src/javascript/JavaScript_Execute_Commands.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class JavaScript_Execute_Commands 
 {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception
 	{
 		//browser initiation
 		WebDriver driver=new ChromeDriver();
@@ -30,6 +30,25 @@ public class JavaScript_Execute_Commands
 		js.executeScript("arguments[0].value='ArjunKrishna@gmail.com'", UserEmail);
 		
 		
+		//Select dropdown option using javascript..  [Option tag value attribute]
+		js.executeScript("document.getElementById('day').value='8'");
+		js.executeScript("document.getElementById('month').value='9'");
+		js.executeScript("document.getElementById('year').value='2000'");
+		Thread.sleep(3000); 
+		
+		//Select Radio button using javascript
+		WebElement Female_radio_btn=driver.findElement(By.xpath("//input[@value='1']"));
+		js.executeScript("arguments[0].checked=true", Female_radio_btn);
+		Thread.sleep(3000);
+		
+		//Select Radio button using javascript
+		WebElement male_radio_btn=driver.findElement(By.xpath("//input[@value='2']"));
+		js.executeScript("arguments[0].click()", male_radio_btn);
+		Thread.sleep(3000);
+		
+		//Identify button using webdriver
+		WebElement Signup_btn=driver.findElement(By.xpath("//button[@name='websubmit']"));
+		js.executeScript("arguments[0].click()", Signup_btn);
 		
 	}
 
